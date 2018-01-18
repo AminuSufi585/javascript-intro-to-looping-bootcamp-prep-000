876d82b04f687d79de60b4c981dc4429c4ddd8de
Aminu Ibrahim Abubakar <aminukano585@gmail.com>
2018-01-18 16:58:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 var arr = [];
 
 function forLoop(arr) {
-   for (var i = 0; i < 25; i++) {
+   for (var i = 0; i < arr.length; i++) {
       arr.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
    }
 }
